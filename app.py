1c7492f935fa1588bca3919a7a37411bfb2f91e0
Walts1991 <walts1991@googlemail.com>
2024-01-15 23:33:50 +0000

    Fix

--- app.py
+++ app.py
@@ -28,3 +28,6 @@ def index():
         return render_template('index.html', result=result)
 
     return render_template('index.html', result=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
