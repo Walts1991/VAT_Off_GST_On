fb30f115424cd14af7fd263003bc1b16eb8b24a3
Walts1991 <walts1991@googlemail.com>
2024-01-15 23:28:04 +0000

    Rounded values

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -12,12 +14,12 @@ def calculate_tax(value):
     gst_amount = gst_added_value - vat_removed_value
 
     return {
-        'original_value': value,
-        'vat_removed_value': vat_removed_value,
-        'vat_amount': vat_amount,
-        'gst_added_value': gst_added_value,
-        'gst_amount': gst_amount,
-        'final_value': gst_added_value
+        'original_value': round(value, 2),
+        'vat_removed_value': round(vat_removed_value, 2),
+        'vat_amount': round(vat_amount, 2),
+        'gst_added_value': round(gst_added_value, 2),
+        'gst_amount': round(gst_amount, 2),
+        'final_value': round(gst_added_value, 2)
     }
 
 @app.route('/', methods=['GET', 'POST'])
